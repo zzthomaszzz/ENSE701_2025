0ffe0674d1cbe7c120cf5ab293e821b3eb684a67
719 <penelope.huang20000@gmail.com>
2025-05-13 21:33:52 +1200

    test

--- front_end/speed_frontend/src/app/page.tsx
+++ front_end/speed_frontend/src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Hello
           </a>
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
